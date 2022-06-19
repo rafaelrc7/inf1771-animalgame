ed3cd305492501e0dc9c3f4ea0e2473a3439c703
rafaelrc7 <contact@rafaelrc.com>
2022-06-19 18:37:45 -0300

    Read strings and chars

--- src/animal.pl
+++ src/animal.pl
@@ -3,30 +3,30 @@
 load_kb :- retractall(node(_, _, _))
 			, (exists_file('animalsgame.kb') , consult('animalsgame.kb'))
 				; consult('animalsgameinit.kb').
-save_kb :- write("Do you want to save (y/n)? ") , read(IN)
-			, ((IN = n , true)
-				; (IN = y , tell('animalsgame.kb') , listing(node) , told)).
+save_kb :- write("Do you want to save (y/n)? ") , read_next_char(IN)
+			, ((IN = 'n' , true)
+				; (IN = 'y' , tell('animalsgame.kb') , listing(node) , told)).
 
 start :- load_kb , (node(Q, Y, N)) , ! , play(node(Q, Y, N)).
 play(N) :- ask_node(N)
 			, (write("Do you want to play again (y/n)? "))
-			, read(IN)
-			, ((IN = y , nl , play(N))
-				; (IN = n , save_kb , nl , write("Thanks for playing!") , nl)).
+			, read_next_char(IN)
+			, ((IN = 'y' , nl , play(N))
+				; (IN = 'n' , save_kb , nl , write("Thanks for playing!") , nl)).
 
-ask_node(node(q(Q), Y, N)) :- write(Q) , write(" (y/n): ") , read(IN)
-	, (IN = y , ((Y = q(_) , node(Y, YY, YN) , ask_node(node(Y, YY, YN)))
+ask_node(node(q(Q), Y, N)) :- write(Q) , write(" (y/n): ") , read_next_char(IN)
+	, (IN = 'y' , ((Y = q(_) , node(Y, YY, YN) , ask_node(node(Y, YY, YN)))
 					; Y = a(_) , ask_animal(Y)))
 		; (((N = q(_) , node(N, NY, NN) , ask_node(node(N, NY, NN)))
 				; N = a(_) , ask_animal(N))).
 
 ask_animal(a(A)) :- write("Is it the ") , write(A) , write("? (y/n): ")
-	, read(IN)
-	, (IN = y , (write("I won!") , nl))
+	, read_next_char(IN)
+	, (IN = 'y' , (write("I won!") , nl))
 		; (write("What is the right animals name? ")
-			, read(NA)
+			, read_string(user_input, "\r\n", "\r\n\t ", _, NA)
 			, write("Type a question to differentiante from it: ")
-			, read(NQ)
+			, read_string(user_input, "\r\n", "\r\n\t ", _, NQ)
 			, write("What would be the answer to get it (y/n)? ")
 			, read(NQA)
 			, ((NQA = y , NN = node(q(NQ), a(NA), a(A)))
@@ -37,4 +37,6 @@ ask_animal(a(A)) :- write("Is it the ") , write(A) , write("? (y/n): ")
 			, assertz(RN)
 			, assertz(NN)
 		).
+
+read_next_char(C) :- get_char(C) , read_pending_chars(user_input, _, _).
 
