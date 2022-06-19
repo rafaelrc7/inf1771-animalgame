0dcdec4308b0c052f2559c1765c03b960f8151e1
rafaelrc7 <contact@rafaelrc.com>
2022-06-19 19:30:20 -0300

    Add basic comments

--- src/animal.pl
+++ src/animal.pl
@@ -1,25 +1,35 @@
 :- dynamic node/3.
 
+% Clears current tree and loads KB from 'animalsgame.kb' if it exists.
+% Otherwise, load the default one from 'animalsgameinit.kb'.
 load_kb :- retractall(node(_, _, _))
 			, (exists_file('animalsgame.kb') , consult('animalsgame.kb'))
 				; consult('animalsgameinit.kb').
+
+% Asks player if he wants to save the KB to a file.
 save_kb :- query_yn("Do you want to save?" , IN)
 			, ((IN = 'n' , true)
 				; (IN = 'y' , tell('animalsgame.kb') , listing(node) , told))
 				; save_kb.
 
+% Runs game, starting with the first node question.
 start :- load_kb , (node(Q, Y, N)) , ! , play(node(Q, Y, N)) , !.
+
+% Game loop, runs until player chooses to exit.
 play(N) :- ask_node(N)
 			, query_yn("Do you want to play again?", IN)
 			, ((IN = 'y' , nl , play(N))
 				; (IN = 'n' , save_kb , nl , write("Thanks for playing!") , nl)).
 
+% Asks node question, and executes action related to the chosen child.
 ask_node(node(q(Q), Y, N)) :- query_yn(Q, IN)
 	, (IN = 'y' , ((Y = q(_) , node(Y, YY, YN) , ask_node(node(Y, YY, YN)))
 					; Y = a(_) , ask_animal(Y)))
 		; (((N = q(_) , node(N, NY, NN) , ask_node(node(N, NY, NN)))
 				; N = a(_) , ask_animal(N))).
 
+% Questions have reached an animal, a tree leaf. Asks with animal is correct,
+% if not registers new question and node.
 ask_animal(a(A)) :- string_concat("Is it the ", A, QYN) , query_yn(QYN, IN)
 	, (IN = 'y' , (write("I won!") , nl))
 		; (write("What is the right animals name? ")
@@ -36,7 +46,10 @@ ask_animal(a(A)) :- string_concat("Is it the ", A, QYN) , query_yn(QYN, IN)
 			, assertz(NN)
 		).
 
+% Reads one char from user_input and discards the rest.
 read_next_char(C) :- get_char(C) , read_pending_chars(user_input, _, _).
+
+% Repeats question until user inputs valid input.
 query_yn(S, C) :- write(S) , write(" (y/n): ") , read_next_char(IN)
 					, ((IN == 'y' ; IN == 'n') -> C = IN ; query_yn(S, C)).
 
