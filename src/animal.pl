8015e2f0b617cffa5c3d1e0ca00b3634f5bc0b72
rafaelrc7 <contact@rafaelrc.com>
2022-06-27 12:51:45 -0300

    Refactor tree structure

--- src/animal.pl
+++ src/animal.pl
@@ -1,55 +1,49 @@
-:- dynamic node/3.
+:- dynamic kb/1.
 
 % Clears current tree and loads KB from 'animalsgame.kb' if it exists.
 % Otherwise, load the default one from 'animalsgameinit.kb'.
-load_kb :- retractall(node(_, _, _))
+load_kb :- retractall(kb(_))
 			, (exists_file('animalsgame.kb') , consult('animalsgame.kb'))
 				; consult('animalsgameinit.kb').
 
 % Asks player if he wants to save the KB to a file.
 save_kb :- query_yn("Do you want to save?" , IN)
 			, ((IN = 'n' , true)
-				; (IN = 'y' , tell('animalsgame.kb') , listing(node) , told))
+				; (IN = 'y' , tell('animalsgame.kb') , listing(kb) , told))
 				; save_kb.
 
 % Runs game, starting with the first node question.
-start :- load_kb , (node(Q, Y, N)) , ! , play(node(Q, Y, N)) , !.
+start :- load_kb , kb(T) , play(T) , !.
 
 % Game loop, runs until player chooses to exit.
-play(N) :- ask_node(N)
+play(T) :- ask_node(T, TN)
+			, (TN \= void -> retractall(kb(_)) , assert(kb(TN)) ; true)
 			, query_yn("Do you want to play again?", IN)
-			, ((IN = 'y' , nl , play(N))
-				; (IN = 'n' , save_kb , nl , write("Thanks for playing!") , nl)).
+			, ((IN = 'y' , nl , kb(TC) , play(TC))
+				; (IN = 'n' , save_kb , format("~nThanks for playing!~n"))).
 
-% Asks node question, and executes action related to the chosen child.
-ask_node(node(q(Q), Y, N)) :- query_yn(Q, IN)
-	, (IN = 'y' , ((Y = q(_) , node(Y, YY, YN) , ask_node(node(Y, YY, YN)))
-					; Y = a(_) , ask_animal(Y)))
-		; (((N = q(_) , node(N, NY, NN) , ask_node(node(N, NY, NN)))
-				; N = a(_) , ask_animal(N))).
+qInsert(a(A), AN, Q, ANS, T) :- (ANS = y) -> (T = tree(Q, AN, a(A)))
+												; (T = tree(Q, a(A), AN)).
 
-% Questions have reached an animal, a tree leaf. Asks with animal is correct,
-% if not registers new question and node.
-ask_animal(a(A)) :- string_concat("Is it the ", A, QYN) , query_yn(QYN, IN)
-	, (IN = 'y' , (write("I won!") , nl))
+ask_node(tree(Q, Y, N), T) :- query_yn(Q, IN)
+	, ((IN = 'y') -> (ask_node(Y, YN)
+						, (YN \= void -> T = tree(Q, YN, N) ; T = void))
+		; (ask_node(N, NN) , (NN \= void -> T = tree(Q, Y, NN) ; T = void))).
+
+ask_node(a(A), N) :- string_concat("Is it the ", A, Q) , query_yn(Q, IN)
+	, (IN = 'y' -> format("I won!~n~n") , N = void)
 		; (write("What is the right animals name? ")
-			, read_string(user_input, "\r\n", "\r\n\t ", _, NA)
-			, write("Type a question to differentiate from it: ")
-			, read_string(user_input, "\r\n", "\r\n\t ", _, NQ)
-			, query_yn("What would be the answer to reach the new animal?", NQA)
-			, ((NQA = y , NN = node(q(NQ), a(NA), a(A)))
-				; ((NQA = n) , NN = node(q(NQ), a(A), a(NA))))
-			, ((ON = node(Q, a(A), N) , RN = node(Q, q(NQ), N))
-				; (ON = node(Q, Y, a(A)) , RN = node(Q, Y, q(NQ))))
-			, retract(ON)
-			, assertz(RN)
-			, assertz(NN)
+			, read_string(user_input, "\r\n", "\r\n\t ", _, AN)
+			, format("Type a question to differentiate it from ~w: ", [A])
+			, read_string(user_input, "\r\n", "\r\n\t ", _, QN)
+			, query_yn("What would be the answer to get the new animal?", QNA)
+			, qInsert(a(A), a(AN), QN, QNA, N)
 		).
 
 % Reads one char from user_input and discards the rest.
 read_next_char(C) :- get_char(C) , read_pending_chars(user_input, _, _).
 
 % Repeats question until user inputs valid input.
-query_yn(S, C) :- write(S) , write(" (y/n): ") , read_next_char(IN)
+query_yn(S, C) :- format("~w (y/n): ", [S]) , read_next_char(IN)
 					, ((IN == 'y' ; IN == 'n') -> C = IN ; query_yn(S, C)).
 
